a7a0dad991588517f2bf8f50192edabb1ade52fa
bobbybabu007 <bobbybabu007@gmail.com>
2021-05-27 20:03:47 -0400

    Generalising DB

--- config1.js
+++ config1.js
@@ -2,10 +2,10 @@ const env = process.env;
 
 const config1 = {
   db: { /* Password and below sensitive info are exposed for demo, PROD launch needs app config and Cloud SQL instances */
-    host: env.DB_HOST || 'us-cdbr-east-03.cleardb.com',
-    user: env.DB_USER || '$$',
-    password: env.DB_PASSWORD || '$$',
-    database: env.DB_NAME || '$$',
+    host: env.DB_HOST || '127.0.0.1',
+    user: env.DB_USER || '${DB_USER}',
+    password: env.DB_PASSWORD || '${DB_PASSWORD}',
+    database: env.DB_NAME || 'titanic',
   },
 
   listPerPage: env.LIST_PER_PAGE || 10,
