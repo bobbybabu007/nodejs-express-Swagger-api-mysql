b4e5d9cf1b3ac9050d284f197129a4b153892250
bobbybabu007 <bobbybabu007@gmail.com>
2021-05-27 20:07:01 -0400

    Generalising DB

--- config1.js
+++ config1.js
@@ -2,7 +2,7 @@ const env = process.env;
 
 const config1 = {
   db: { /* Password and below sensitive info are exposed for demo, PROD launch needs app config and Cloud SQL instances */
-    host: env.DB_HOST || '127.0.0.1',
+    host: env.DB_HOST || 'localhost',
     user: env.DB_USER || '${DB_USER}',
     password: env.DB_PASSWORD || '${DB_PASSWORD}',
     database: env.DB_NAME || 'titanic',
